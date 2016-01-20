1d3b614b800d5f2e9ee0b768a85644001cea29d1
martnga <mtnjoroge@gmail.com>
2016-01-20 19:56:16 +0300

    Swipe Refresh

---
+++ SwipeRefresh/app/src/main/java/org/mansa/swiperefresh/helper/Movie.java
@@ -0,0 +1,18 @@
+package org.mansa.swiperefresh.helper;
+
+/**
+ * Created by mansa on 1/20/16.
+ */
+public class Movie {
+    public int id;
+    public String title;
+
+    public Movie() {
+    }
+
+    public Movie(int id, String title) {
+        this.title = title;
+        this.id = id;
+    }
+}
+
